5722da314b566f1ced90c9da4487a8f45bddb55b
Jacson Siqueira Cesar <jacson.cesar@alunos.utfpr.edu.br>
2023-12-03 20:17:13 -0300

    Trabalho final POO2 Chat (Ajustes NetBeans)

--- Servidor/src/main/java/com/mycompany/servidor/Participante.java
+++ Servidor/src/main/java/com/mycompany/servidor/Participante.java
@@ -1,3 +1,5 @@
+package com.mycompany.servidor;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- Servidor/src/main/java/com/mycompany/servidor/ServicoMensagem.java
+++ Servidor/src/main/java/com/mycompany/servidor/ServicoMensagem.java
@@ -1,3 +1,5 @@
+package com.mycompany.servidor;
+
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.logging.Level;
